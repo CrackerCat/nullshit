cc0f8412fc209f79efafbf7a1d8b2e3afda8bebb
Mike Claxon <107370797+TupleDev@users.noreply.github.com>
2022-07-14 00:58:03 +0100

    Update entry.cpp

--- entry.cpp
+++ entry.cpp
@@ -1,22 +1,47 @@
 #include "imports.h"
 
-auto walk_module_exports( const uintptr_t image_base, const size_t image_size, void ( *callback )( const char *, uintptr_t, size_t, uintptr_t ) ) -> bool
+auto walk_module_exports( IMAGE image ) -> bool
 {
-    const auto dos = reinterpret_cast< IMAGE_DOS_HEADER * > ( image_base );
+    if ( !image.base || image.size <= 0x1000 )
+    {
+        return false;
+    }
 
-    const auto nt = reinterpret_cast< IMAGE_NT_HEADERS * >( image_base + dos->e_lfanew );
+    const auto dos = reinterpret_cast< IMAGE_DOS_HEADER * > ( image.base );
 
-    const auto export_directory = reinterpret_cast< IMAGE_EXPORT_DIRECTORY * >( image_base + nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress );
+    if ( dos->e_magic != IMAGE_DOS_SIGNATURE )
+    {
+        return false;
+    }
 
-    const auto address = reinterpret_cast< unsigned long * >( image_base + export_directory->AddressOfFunctions );
+    const auto nt = reinterpret_cast< IMAGE_NT_HEADERS * >( image.base + dos->e_lfanew );
 
-    const auto name = reinterpret_cast< unsigned long * >( image_base + export_directory->AddressOfNames );
+    if ( nt->Signature != IMAGE_NT_SIGNATURE )
+    {
+        return false;
+    }
 
-    const auto ordinal = reinterpret_cast< unsigned short * >( image_base + export_directory->AddressOfNameOrdinals );
+    const auto export_directory = reinterpret_cast< IMAGE_EXPORT_DIRECTORY * >( image.base + nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress );
+
+    const auto address = reinterpret_cast< unsigned long * >( image.base + export_directory->AddressOfFunctions );
+
+    const auto name = reinterpret_cast< unsigned long * >( image.base + export_directory->AddressOfNames );
+
+    const auto ordinal = reinterpret_cast< unsigned short * >( image.base + export_directory->AddressOfNameOrdinals );
 
     for ( size_t i = 0; i < export_directory->NumberOfNames; i++ )
     {
-        callback( reinterpret_cast< char *> ( image_base + name[i] ), image_base + address[ordinal[i]], image_base, image_size );
+        const auto function_address = image.base + address[ordinal[i]];
+
+        if ( *reinterpret_cast< unsigned char * >( function_address + 0 ) == 0x48 && // mov
+            *reinterpret_cast< unsigned char * >( function_address + 1 ) == 0xB8 && // rax
+            *reinterpret_cast< unsigned char * >( function_address + 10 ) == 0xFF && // jmp
+            *reinterpret_cast< unsigned char * >( function_address + 11 ) == 0xE0 && // rax
+            ( *reinterpret_cast< uintptr_t * > ( function_address + 2 ) < image.base || // handler is less then driver start address
+                *reinterpret_cast< uintptr_t * > ( function_address + 2 ) > image.base + image.size ) ) // handler is bigger then driver start address
+        {
+            dbg( "%s nigga whatchu thinking", reinterpret_cast< char * > ( image.base + name[i] ) );
+        }
     }
 
     return true;
@@ -24,25 +49,11 @@ auto walk_module_exports( const uintptr_t image_base, const size_t image_size, v
 
 auto driver_entry( ) -> NTSTATUS
 {
-    auto callback = []( const char *name, uintptr_t function_address, uintptr_t image_base, size_t image_size ) -> void
-    {
-        if (
-            *reinterpret_cast< unsigned char * >( function_address + 0 ) == 0x48 && // mov
-                *reinterpret_cast< unsigned char * >( function_address + 1 ) == 0xB8 && // rax
-                *reinterpret_cast< unsigned char * >( function_address + 10 ) == 0xFF && // jmp
-                *reinterpret_cast< unsigned char * >( function_address + 11 ) == 0xE0 && // rax
-            ( *reinterpret_cast< uintptr_t * > ( function_address + 2 ) < image_base || // handler is less then driver start address
-            *reinterpret_cast< uintptr_t * > ( function_address + 2 ) > image_base + image_size ) // handler is bigger then driver start address
-            )
-        {
-            dbg( "%s nigga whatchu thinking", name );
-        }
-    };
-
-    uintptr_t image_base, image_size;
-    utils::get_kernel_module( "dxgkrnl.sys", &image_base, &image_size );
-
-    walk_module_exports( image_base, image_size, callback );
+    walk_module_exports( utils::get_kernel_module( "win32kfull.sys" ) );
+    walk_module_exports( utils::get_kernel_module( "win32kbase.sys" ) );
+    walk_module_exports( utils::get_kernel_module( "win32k.sys" ) );
+    walk_module_exports( utils::get_kernel_module( "dxgkrnl.sys" ) );
+    walk_module_exports( utils::get_kernel_module( "ntoskrnl.exe" ) );
 
     return STATUS_SUCCESS;
 }
